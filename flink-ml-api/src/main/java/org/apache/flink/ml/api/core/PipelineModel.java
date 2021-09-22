5ff346ea1a508a00b89759492f09e7330e69baef
Dong Lin <lindong28@gmail.com>
2021-09-22 13:47:39 +0800

    [FLINK-22915][FLIP-173] Update Flink ML API to support AlgoOperator with multiple input tables and multiple output tables

---
+++ flink-ml-api/src/main/java/org/apache/flink/ml/api/core/PipelineModel.java
@@ -0,0 +1,83 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.flink.ml.api.core;
+
+import org.apache.flink.annotation.PublicEvolving;
+import org.apache.flink.annotation.VisibleForTesting;
+import org.apache.flink.ml.api.misc.param.Params;
+import org.apache.flink.table.api.Table;
+
+import java.io.IOException;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * A PipelineModel acts as a Model. It consists of an ordered list of stages, each of which could be
+ * a Model, Transformer or AlgoOperator.
+ */
+@PublicEvolving
+public final class PipelineModel implements Model<PipelineModel> {
+    private static final long serialVersionUID = 6184950154217411318L;
+    private final List<Stage<?>> stages;
+    private final Params params = new Params();
+
+    public PipelineModel(List<Stage<?>> stages) {
+        this.stages = stages;
+    }
+
+    /**
+     * Applies all stages in this PipelineModel on the input tables in order. The output of one
+     * stage is used as the input of the next stage (if any). The output of the last stage is
+     * returned as the result of this method.
+     *
+     * @param inputs a list of tables
+     * @return a list of tables
+     */
+    @Override
+    public Table[] transform(Table... inputs) {
+        for (Stage<?> stage : stages) {
+            inputs = ((AlgoOperator<?>) stage).transform(inputs);
+        }
+        return inputs;
+    }
+
+    @Override
+    public void save(String path) throws IOException {
+        throw new UnsupportedOperationException();
+    }
+
+    public static PipelineModel load(String path) throws IOException {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public Params getParams() {
+        return params;
+    }
+
+    /**
+     * Returns a list of all stages in this PipelineModel in order. The list is immutable.
+     *
+     * @return an immutable list of transformers.
+     */
+    @VisibleForTesting
+    List<Stage<?>> getStages() {
+        return Collections.unmodifiableList(stages);
+    }
+}
---
+++ flink-ml-api/src/main/java/org/apache/flink/ml/api/core/Stage.java
@@ -0,0 +1,44 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.flink.ml.api.core;
+
+import org.apache.flink.annotation.PublicEvolving;
+import org.apache.flink.ml.api.misc.param.WithParams;
+
+import java.io.IOException;
+import java.io.Serializable;
+
+/**
+ * Base class for a node in a Pipeline or Graph. The interface is only a concept, and does not have
+ * any actual functionality. Its subclasses could be Estimator, Model, Transformer or AlgoOperator.
+ * No other classes should inherit this interface directly.
+ *
+ * <p>Each stage is with parameters, and requires a public empty constructor for restoration.
+ *
+ * @param <T> The class type of the Stage implementation itself.
+ */
+@PublicEvolving
+public interface Stage<T extends Stage<T>> extends WithParams<T>, Serializable {
+    /** Saves this stage to the given path. */
+    void save(String path) throws IOException;
+
+    // NOTE: every Stage subclass should implement a static method with signature "static T
+    // load(String path)", where T refers to the concrete subclass. This static method should
+    // instantiate a new stage instance based on the data from the given path.
+}
