e8019371c6693a653126a456e30521cc1c93e2b9
Yun Gao <gaoyunhenhao@gmail.com>
2021-09-23 16:24:15 +0800

    [FLINK-24644][iteration] Introduce broadcast output to support broadcast events to all the downstream operators

---
+++ flink-ml-iteration/src/main/java/org/apache/flink/iteration/utils/ReflectionUtils.java
@@ -0,0 +1,52 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.flink.iteration.utils;
+
+import java.lang.reflect.Field;
+
+/** Utility class to provide some reflection tools. */
+public class ReflectionUtils {
+
+    public static Field getClassField(Class<?> declaredClass, String fieldName) {
+        try {
+            Field field = declaredClass.getDeclaredField(fieldName);
+            field.setAccessible(true);
+            return field;
+        } catch (Exception e) {
+            throw new RuntimeException(
+                    "Failed to get field" + fieldName + " from " + declaredClass, e);
+        }
+    }
+
+    @SuppressWarnings("unchecked")
+    public static <T> T getFieldValue(Object targetObject, Field field) {
+        try {
+            return (T) field.get(targetObject);
+        } catch (Exception e) {
+            throw new RuntimeException(
+                    "Failed to get field " + field.getName() + " from " + targetObject, e);
+        }
+    }
+
+    public static <T> T getFieldValue(
+            Object targetObject, Class<?> declaredClass, String fieldName) {
+        Field field = getClassField(declaredClass, fieldName);
+        return getFieldValue(targetObject, field);
+    }
+}
