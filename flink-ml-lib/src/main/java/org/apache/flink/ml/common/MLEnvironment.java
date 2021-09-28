85c5f1b0843b7cbf13d968c5b4ba7866fec04894
zhangzp <zhangzhipengw@gmail.com>
2021-09-28 18:01:03 +0800

    [FLINK-24279][hotfix] upgrade to FLINK1.14 and delete outdated codes about dataset

--- flink-ml-lib/src/main/java/org/apache/flink/ml/common/MLEnvironment.java
+++ flink-ml-lib/src/main/java/org/apache/flink/ml/common/MLEnvironment.java
@@ -22,7 +22,6 @@ package org.apache.flink.ml.common;
 import org.apache.flink.api.java.ExecutionEnvironment;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.table.api.EnvironmentSettings;
-import org.apache.flink.table.api.bridge.java.BatchTableEnvironment;
 import org.apache.flink.table.api.bridge.java.StreamTableEnvironment;
 
 /**
@@ -34,30 +33,15 @@ import org.apache.flink.table.api.bridge.java.StreamTableEnvironment;
  *
  * @see ExecutionEnvironment
  * @see StreamExecutionEnvironment
- * @see BatchTableEnvironment
  * @see StreamTableEnvironment
  */
 public class MLEnvironment {
-    private ExecutionEnvironment env;
     private StreamExecutionEnvironment streamEnv;
-    private BatchTableEnvironment batchTableEnv;
     private StreamTableEnvironment streamTableEnv;
 
     /** Construct with null that the class can load the environment in the `get` method. */
     public MLEnvironment() {
-        this(null, null, null, null);
-    }
-
-    /**
-     * Construct with the batch environment and the the batch table environment given by user.
-     *
-     * <p>The env can be null which will be loaded in the `get` method.
-     *
-     * @param batchEnv the ExecutionEnvironment
-     * @param batchTableEnv the BatchTableEnvironment
-     */
-    public MLEnvironment(ExecutionEnvironment batchEnv, BatchTableEnvironment batchTableEnv) {
-        this(batchEnv, batchTableEnv, null, null);
+        this(null, null);
     }
 
     /**
@@ -70,41 +54,8 @@ public class MLEnvironment {
      */
     public MLEnvironment(
             StreamExecutionEnvironment streamEnv, StreamTableEnvironment streamTableEnv) {
-        this(null, null, streamEnv, streamTableEnv);
-    }
-
-    /**
-     * Construct with env given by user.
-     *
-     * <p>The env can be null which will be loaded in the `get` method.
-     *
-     * @param batchEnv the ExecutionEnvironment
-     * @param batchTableEnv the BatchTableEnvironment
-     * @param streamEnv the StreamExecutionEnvironment
-     * @param streamTableEnv the StreamTableEnvironment
-     */
-    public MLEnvironment(
-            ExecutionEnvironment batchEnv,
-            BatchTableEnvironment batchTableEnv,
-            StreamExecutionEnvironment streamEnv,
-            StreamTableEnvironment streamTableEnv) {
-        this.env = batchEnv;
-        this.batchTableEnv = batchTableEnv;
         this.streamEnv = streamEnv;
         this.streamTableEnv = streamTableEnv;
-    }
-
-    /**
-     * Get the ExecutionEnvironment. if the ExecutionEnvironment has not been set, it initial the
-     * ExecutionEnvironment with default Configuration.
-     *
-     * @return the batch {@link ExecutionEnvironment}
-     */
-    public ExecutionEnvironment getExecutionEnvironment() {
-        if (null == env) {
-            env = ExecutionEnvironment.getExecutionEnvironment();
-        }
-        return env;
     }
 
     /**
@@ -121,19 +72,6 @@ public class MLEnvironment {
     }
 
     /**
-     * Get the BatchTableEnvironment. if the BatchTableEnvironment has not been set, it initial the
-     * BatchTableEnvironment with default Configuration.
-     *
-     * @return the {@link BatchTableEnvironment}
-     */
-    public BatchTableEnvironment getBatchTableEnvironment() {
-        if (null == batchTableEnv) {
-            batchTableEnv = BatchTableEnvironment.create(getExecutionEnvironment());
-        }
-        return batchTableEnv;
-    }
-
-    /**
      * Get the StreamTableEnvironment. if the StreamTableEnvironment has not been set, it initial
      * the StreamTableEnvironment with default Configuration.
      *
@@ -144,7 +82,7 @@ public class MLEnvironment {
             streamTableEnv =
                     StreamTableEnvironment.create(
                             getStreamExecutionEnvironment(),
-                            EnvironmentSettings.newInstance().useOldPlanner().build());
+                            EnvironmentSettings.newInstance().build());
         }
         return streamTableEnv;
     }
