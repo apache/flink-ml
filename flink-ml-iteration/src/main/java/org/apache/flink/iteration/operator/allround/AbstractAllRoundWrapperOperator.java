be41863ea8242584d1ca78e44401bad9f892878f
Yun Gao <gaoyunhenhao@gmail.com>
2021-10-07 01:28:27 +0800

    [FLINK-24655][iteration] Skip the repeat round for all-round operator

--- flink-ml-iteration/src/main/java/org/apache/flink/iteration/operator/allround/AbstractAllRoundWrapperOperator.java
+++ flink-ml-iteration/src/main/java/org/apache/flink/iteration/operator/allround/AbstractAllRoundWrapperOperator.java
@@ -18,28 +18,45 @@
 
 package org.apache.flink.iteration.operator.allround;
 
+import org.apache.flink.annotation.VisibleForTesting;
+import org.apache.flink.api.common.state.ListState;
+import org.apache.flink.api.common.state.ListStateDescriptor;
+import org.apache.flink.api.common.state.OperatorStateStore;
+import org.apache.flink.api.common.typeutils.TypeSerializer;
+import org.apache.flink.api.common.typeutils.base.IntSerializer;
+import org.apache.flink.core.fs.CloseableRegistry;
 import org.apache.flink.iteration.IterationListener;
 import org.apache.flink.iteration.IterationRecord;
 import org.apache.flink.iteration.operator.AbstractWrapperOperator;
+import org.apache.flink.iteration.operator.OperatorStateUtils;
+import org.apache.flink.metrics.MetricGroup;
 import org.apache.flink.metrics.groups.OperatorMetricGroup;
 import org.apache.flink.runtime.checkpoint.CheckpointOptions;
 import org.apache.flink.runtime.jobgraph.OperatorID;
 import org.apache.flink.runtime.state.CheckpointStreamFactory;
+import org.apache.flink.streaming.api.operators.KeyContext;
 import org.apache.flink.streaming.api.operators.OperatorSnapshotFutures;
 import org.apache.flink.streaming.api.operators.Output;
 import org.apache.flink.streaming.api.operators.StreamOperator;
 import org.apache.flink.streaming.api.operators.StreamOperatorFactory;
 import org.apache.flink.streaming.api.operators.StreamOperatorFactoryUtil;
 import org.apache.flink.streaming.api.operators.StreamOperatorParameters;
+import org.apache.flink.streaming.api.operators.StreamOperatorStateContext;
 import org.apache.flink.streaming.api.operators.StreamTaskStateInitializer;
 import org.apache.flink.streaming.api.operators.TimestampedCollector;
 import org.apache.flink.streaming.runtime.streamrecord.StreamRecord;
+import org.apache.flink.streaming.runtime.tasks.ProcessingTimeService;
 import org.apache.flink.streaming.runtime.tasks.StreamTask;
 import org.apache.flink.util.OutputTag;
 
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+
 import java.io.IOException;
+import java.util.Collections;
 
 import static org.apache.flink.iteration.operator.OperatorUtils.processOperatorOrUdfIfSatisfy;
+import static org.apache.flink.util.Preconditions.checkState;
 
 /** The base class for the all-round wrapper operators. */
 public abstract class AbstractAllRoundWrapperOperator<T, S extends StreamOperator<T>>
@@ -48,6 +65,13 @@ public abstract class AbstractAllRoundWrapperOperator<T, S extends StreamOperato
     protected final S wrappedOperator;
 
     private final IterationContext iterationContext;
+
+    // --------------- state ---------------------------
+    private int latestEpochWatermark = -1;
+
+    private ListState<Integer> parallelismState;
+
+    private ListState<Integer> latestEpochWatermarkState;
 
     @SuppressWarnings({"unchecked", "rawtypes"})
     public AbstractAllRoundWrapperOperator(
@@ -75,6 +99,11 @@ public abstract class AbstractAllRoundWrapperOperator<T, S extends StreamOperato
 
     @Override
     public void onEpochWatermarkIncrement(int epochWatermark) throws IOException {
+        if (epochWatermark <= latestEpochWatermark) {
+            return;
+        }
+        latestEpochWatermark = epochWatermark;
+
         setIterationContextRound(epochWatermark);
         processOperatorOrUdfIfSatisfy(
                 wrappedOperator,
@@ -100,6 +129,69 @@ public abstract class AbstractAllRoundWrapperOperator<T, S extends StreamOperato
     }
 
     @Override
+    public void initializeState(StreamTaskStateInitializer streamTaskStateManager)
+            throws Exception {
+        RecordingStreamTaskStateInitializer recordingStreamTaskStateInitializer =
+                new RecordingStreamTaskStateInitializer(streamTaskStateManager);
+        wrappedOperator.initializeState(recordingStreamTaskStateInitializer);
+        checkState(recordingStreamTaskStateInitializer.lastCreated != null);
+
+        OperatorStateStore operatorStateStore =
+                recordingStreamTaskStateInitializer.lastCreated.operatorStateBackend();
+
+        parallelismState =
+                operatorStateStore.getUnionListState(
+                        new ListStateDescriptor<>("parallelism", IntSerializer.INSTANCE));
+        OperatorStateUtils.getUniqueElement(parallelismState, "parallelism")
+                .ifPresent(
+                        oldParallelism ->
+                                checkState(
+                                        oldParallelism
+                                                == containingTask
+                                                        .getEnvironment()
+                                                        .getTaskInfo()
+                                                        .getNumberOfParallelSubtasks(),
+                                        "The all-round wrapper operator is recovered with parallelism changed from "
+                                                + oldParallelism
+                                                + " to "
+                                                + containingTask
+                                                        .getEnvironment()
+                                                        .getTaskInfo()
+                                                        .getNumberOfParallelSubtasks()));
+
+        latestEpochWatermarkState =
+                operatorStateStore.getListState(
+                        new ListStateDescriptor<>("latestEpoch", IntSerializer.INSTANCE));
+        OperatorStateUtils.getUniqueElement(latestEpochWatermarkState, "latestEpoch")
+                .ifPresent(
+                        oldLatestEpochWatermark -> latestEpochWatermark = oldLatestEpochWatermark);
+    }
+
+    @Override
+    public OperatorSnapshotFutures snapshotState(
+            long checkpointId,
+            long timestamp,
+            CheckpointOptions checkpointOptions,
+            CheckpointStreamFactory storageLocation)
+            throws Exception {
+
+        // Always clear the union list state before set value.
+        parallelismState.clear();
+        if (containingTask.getEnvironment().getTaskInfo().getIndexOfThisSubtask() == 0) {
+            parallelismState.update(
+                    Collections.singletonList(
+                            containingTask
+                                    .getEnvironment()
+                                    .getTaskInfo()
+                                    .getNumberOfParallelSubtasks()));
+        }
+        latestEpochWatermarkState.update(Collections.singletonList(latestEpochWatermark));
+
+        return wrappedOperator.snapshotState(
+                checkpointId, timestamp, checkpointOptions, storageLocation);
+    }
+
+    @Override
     public void open() throws Exception {
         wrappedOperator.open();
     }
@@ -117,23 +209,6 @@ public abstract class AbstractAllRoundWrapperOperator<T, S extends StreamOperato
     @Override
     public void prepareSnapshotPreBarrier(long checkpointId) throws Exception {
         wrappedOperator.prepareSnapshotPreBarrier(checkpointId);
-    }
-
-    @Override
-    public OperatorSnapshotFutures snapshotState(
-            long checkpointId,
-            long timestamp,
-            CheckpointOptions checkpointOptions,
-            CheckpointStreamFactory storageLocation)
-            throws Exception {
-        return wrappedOperator.snapshotState(
-                checkpointId, timestamp, checkpointOptions, storageLocation);
-    }
-
-    @Override
-    public void initializeState(StreamTaskStateInitializer streamTaskStateManager)
-            throws Exception {
-        wrappedOperator.initializeState(streamTaskStateManager);
     }
 
     @Override
@@ -176,11 +251,53 @@ public abstract class AbstractAllRoundWrapperOperator<T, S extends StreamOperato
         return wrappedOperator.getCurrentKey();
     }
 
+    @VisibleForTesting
+    int getLatestEpochWatermark() {
+        return latestEpochWatermark;
+    }
+
     private class IterationContext implements IterationListener.Context {
 
         @Override
         public <X> void output(OutputTag<X> outputTag, X value) {
             proxyOutput.collect(outputTag, new StreamRecord<>(value));
+        }
+    }
+
+    private static class RecordingStreamTaskStateInitializer implements StreamTaskStateInitializer {
+
+        private final StreamTaskStateInitializer wrapped;
+
+        StreamOperatorStateContext lastCreated;
+
+        public RecordingStreamTaskStateInitializer(StreamTaskStateInitializer wrapped) {
+            this.wrapped = wrapped;
+        }
+
+        @Override
+        public StreamOperatorStateContext streamOperatorStateContext(
+                @Nonnull OperatorID operatorID,
+                @Nonnull String s,
+                @Nonnull ProcessingTimeService processingTimeService,
+                @Nonnull KeyContext keyContext,
+                @Nullable TypeSerializer<?> typeSerializer,
+                @Nonnull CloseableRegistry closeableRegistry,
+                @Nonnull MetricGroup metricGroup,
+                double v,
+                boolean b)
+                throws Exception {
+            lastCreated =
+                    wrapped.streamOperatorStateContext(
+                            operatorID,
+                            s,
+                            processingTimeService,
+                            keyContext,
+                            typeSerializer,
+                            closeableRegistry,
+                            metricGroup,
+                            v,
+                            b);
+            return lastCreated;
         }
     }
 }
