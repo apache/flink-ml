58fce034a36d0e2416f03c61a4de450cc709bfc8
zhangzp <zhangzhipengw@gmail.com>
2022-06-20 10:02:59 +0800

    [FLINK-27877] Add benchmark configuration for StringIndexer, StandardScaler and Bucketizer

---
+++ flink-ml-benchmark/src/main/java/org/apache/flink/ml/benchmark/datagenerator/common/DoubleGenerator.java
@@ -0,0 +1,58 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.flink.ml.benchmark.datagenerator.common;
+
+import org.apache.flink.api.common.typeinfo.TypeInformation;
+import org.apache.flink.api.common.typeinfo.Types;
+import org.apache.flink.api.java.typeutils.RowTypeInfo;
+import org.apache.flink.types.Row;
+import org.apache.flink.util.Preconditions;
+
+import java.util.Arrays;
+
+/** A DataGenerator which creates a table of doubles. */
+public class DoubleGenerator extends InputTableGenerator<DoubleGenerator> {
+
+    @Override
+    protected RowGenerator[] getRowGenerators() {
+        String[][] colNames = getColNames();
+        Preconditions.checkState(colNames.length == 1);
+        int numOutputCols = colNames[0].length;
+
+        return new RowGenerator[] {
+            new RowGenerator(getNumValues(), getSeed()) {
+                @Override
+                public Row nextRow() {
+                    Row r = new Row(numOutputCols);
+                    for (int i = 0; i < numOutputCols; i++) {
+                        r.setField(i, random.nextDouble());
+                    }
+                    return r;
+                }
+
+                @Override
+                protected RowTypeInfo getRowTypeInfo() {
+                    TypeInformation[] outputTypes = new TypeInformation[colNames[0].length];
+                    Arrays.fill(outputTypes, Types.DOUBLE);
+                    return new RowTypeInfo(outputTypes, colNames[0]);
+                }
+            }
+        };
+    }
+}
