132cca8f58d6b446a367d30ee2ff0037870a3cfb
Fan Hong <hongfanxo@gmail.com>
2023-03-01 14:46:37 +0800

    Improve javadoc for GBTClassifier and GBTRegressor.

--- flink-ml-lib/src/main/java/org/apache/flink/ml/regression/gbtregressor/GBTRegressor.java
+++ flink-ml-lib/src/main/java/org/apache/flink/ml/regression/gbtregressor/GBTRegressor.java
@@ -36,7 +36,23 @@ import java.util.Map;
 
 import static org.apache.flink.table.api.Expressions.$;
 
-/** An Estimator which implements the gradient boosting trees regression algorithm. */
+/**
+ * An Estimator which implements the gradient boosting trees regression algorithm (<a
+ * href="http://en.wikipedia.org/wiki/Gradient_boosting">Gradient Boosting</a>).
+ *
+ * <p>The implementation has been inspired by advanced implementations like <a
+ * href="https://www.kdd.org/kdd2016/papers/files/rfp0697-chenAemb.pdf">XGBoost</a> and <a
+ * href="https://proceedings.neurips.cc/paper/2017/file/6449f44a102fde848669bdd9eb6b76fa-Paper.pdf">LightGBM</a>.
+ * It supports features like regularized learning objective with second-order approximation,
+ * histogram-based and sparsity-aware split-finding algorithm.
+ *
+ * <p>The implementation of distributed system takes <a
+ * href="http://www.vldb.org/pvldb/vol12/p1357-fu.pdf">this work</a> as a reference. Right now, we
+ * support horizontal partition of data and row-store storage of instances.
+ *
+ * <p>NOTE: Currently, some features are not supported yet: weighted input samples, early-stopping
+ * with validation set, encoding with leaf ids, etc.
+ */
 public class GBTRegressor
         implements Estimator<GBTRegressor, GBTRegressorModel>, GBTRegressorParams<GBTRegressor> {
 
